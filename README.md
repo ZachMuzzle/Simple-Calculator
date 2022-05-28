cab7bedbc46b3d1a63754e851e1372135ce12d4b
Zachary Muzzleman <zach12342@me.com>
2022-05-28 18:44:30 -0400

    Added files, errors in code that I need to look into as output/history is not displayed.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Calculator Using Javascript
+
+* This is a simple calculator using vanilla Javascript, CSS, HTML. 
+* This is being done for practice.
---
+++ scripts.js
@@ -0,0 +1,89 @@
+/* HISTORY FUNCTION */
+
+function getHistory() {
+    return document.getElementById("history-value").innerText
+}
+
+function printHistory(num) {
+    document.getElementById("history-value").innerText=num;
+}
+
+/* OUTPUT FUNCTIONS TO RETRIEVE AND DISPLAY THE USERS RESULTS */
+
+function getOutput() {
+    return document.getElementById("output-value").innerText;
+}
+
+function printOutput(num) {
+    if (num == " ") {
+        document.getElementById("output-value").innerText=num;
+    }
+    else {
+        document.getElementById("output-value").innerText=getFormattedNumber(num);
+    }
+}
+
+function getFormattedNumber(num) {
+    if(num = "-") {
+        return "";
+    }
+
+    let n = Number(num);
+    let value = n.toLocaleString("en");
+    return value;
+}
+
+/* FUNCTION TO CLEAR COMS TO OUTPUT FIELD */
+function reverseNumberFormat(num) {
+    return Number(num.replace(/,/g,''));
+}
+
+let operator = document.getElementsByClassName("operator");
+for(let i = 0; i < operator.length; i++) {
+    operator[i].addEventListener("click", function() {
+        if(this.id == "clear") {
+            printHistory("");
+            printOutput("");
+        } else if(this.id == "backspace") {
+            let output = reverseNumberFormat(getOutput()).toString();
+            if(output) { //if output has a value
+                output = output.substr(0, output.length-1);
+                printOutput(output)
+            }
+        } else {
+            let output = getOutput();
+            let history = getHistory();
+            if(output == "" && history != "") {
+                if(isNaN(history(history.length-1))) { // isNaN return true or false if Not A Number
+                    history = history.substr(0, history.length-1);
+                }
+            }
+            if(output != "" || history != "" ) {
+                output = output == ""?
+                output:reverseNumberFormat(output);
+                history += output;
+                if(this.id == "=") {
+                    let result = eval(history); //eval - evaluates javascript code and executes it
+                    printOutput(result);
+                    printHistory("");
+                } else {
+                    history += this.id;
+                    printHistory(history);
+                    printOutput("")
+                }
+            }
+        }
+    });
+}
+
+let number = document.getElementsByClassName("number");
+for(let i = 0; i < number.length; i++) {
+    number[i].addEventListener("click", function() {
+        let output = reverseNumberFormat(getOutput());
+        if (output != NaN) { //if output is a number
+            output = operator + this.id;
+            console.log(output);
+            printOutput(output);
+        }
+    });
+}
